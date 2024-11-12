2dfa6c11edc6d4120abff67e28e09bb78a59ccee
Anasstasiya Sviridenkova <anastasia_sviridenkova@mail.ru>
2024-11-12 17:27:08 +0500

    read & parse JSON file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.6")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
+
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,10 +5,18 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
          description = "Compares two configuration files and shows a difference.")
-public class App {
-
+public class App implements Callable<Integer> {
     @Option(names = {"-f", "--format"}, description = "output format", paramLabel = "format")
     private String format = "stylish";
 
@@ -18,8 +26,31 @@ public class App {
     @Parameters(index = "1", description = "path to second file")
     private String filePath2;
 
-    public static void main(String[] args) {
+
+    public Integer call() throws Exception {
+        Path path1 = Paths.get(filePath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filePath2).toAbsolutePath().normalize();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> value1 = mapper.readValue(Files.readAllBytes(path1), new TypeReference<Map<String,Object>>(){});
+        printMap(value1);
+        Map<String, Object> value2 = mapper.readValue(Files.readAllBytes(path2), new TypeReference<Map<String,Object>>(){});
+        printMap(value2);
+
+        return 0;
+    }
+
+    public void printMap(Map<String, Object> map) {
+        System.out.println("{");
+        map.forEach((key, value) -> {
+            System.out.println("  \"" + key + "\": " + value);
+        });
+        System.out.println("}");
+    }
+
+    public static void main(String[] args) throws Exception{
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
+
     }
 }
