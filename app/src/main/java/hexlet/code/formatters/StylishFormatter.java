cedb19ec339db0a13625aab5af47b93fab5f06c5
Anasstasiya Sviridenkova <anastasia_sviridenkova@mail.ru>
2025-08-12 17:36:47 +0500

    mentor check #1

--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -16,23 +16,23 @@ public class StylishFormatter implements DiffFormatter {
             var obj2 = entry.getValue2();
 
             if (operation.equals("added")) {
-                result.append(" + ");
+                result.append("  + ");
                 result.append(key).append(": ").append(obj2);
                 result.append("\n");
             } else if (operation.equals("deleted")) {
-                result.append(" - ");
+                result.append("  - ");
                 result.append(key).append(": ").append(obj1);
                 result.append("\n");
             } else if (operation.equals("changed")) {
-                result.append(" - ");
+                result.append("  - ");
                 result.append(key).append(": ").append(obj1);
                 result.append("\n");
 
-                result.append(" + ");
+                result.append("  + ");
                 result.append(key).append(": ").append(obj2);
                 result.append("\n");
             } else if (operation.equals("unchanged")) {
-                result.append("   ");
+                result.append("    ");
                 result.append(key).append(": ").append(obj1);
                 result.append("\n");
             }
